01238a5c5eb57200fcaab2b10b60ad983152998e
Moad Eltabu <moadeltabu@hotmail.com>
2016-04-24 22:17:53 -0400

    Added AppBar includes back button and Search Box

--- Logins Management System/Logins Management System/Views/MasterPage.xaml.cs
+++ Logins Management System/Logins Management System/Views/MasterPage.xaml.cs
@@ -72,21 +72,23 @@ namespace LoginsManagementSystem.Views
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            SignIn dialog = new SignIn();
-            await dialog.ShowAsync();
 
-            if (dialog.Result == SignInResult.SignInOK)
-            {
-                this.MainFrame.Navigate(typeof(MainPage));
-            }
-            else if (dialog.Result == SignInResult.SignInCancel)
-            {
-                Application.Current.Exit();
-            }
-            else if (dialog.Result == SignInResult.Nothing)
-            {
-                Application.Current.Exit();
-            }
+            //SignIn dialog = new SignIn();
+            //await dialog.ShowAsync();
+
+            //if (dialog.Result == SignInResult.SignInOK)
+            //{
+            //    this.MainFrame.Navigate(typeof(MainPage));
+            //}
+            //else if (dialog.Result == SignInResult.SignInCancel)
+            //{
+            //    Application.Current.Exit();
+            //}
+            //else if (dialog.Result == SignInResult.Nothing)
+            //{
+            //    Application.Current.Exit();
+            //}
+
         }
 
         private void ThemeToggleButton_Checked(object sender, RoutedEventArgs e)
@@ -97,6 +99,81 @@ namespace LoginsManagementSystem.Views
         private void ThemeToggleButton_Unchecked(object sender, RoutedEventArgs e)
         {
             this.RequestedTheme = ElementTheme.Dark;
+        }
+
+        /// <summary>
+        /// This event gets fired when:
+        ///     * a user presses Enter while focus is in the TextBox
+        ///     * a user clicks or tabs to and invokes the query button (defined using the QueryIcon API)
+        ///     * a user presses selects (clicks/taps/presses Enter) a suggestion
+        /// </summary>
+        /// <param name="sender">The AutoSuggestBox that fired the event.</param>
+        /// <param name="args">The args contain the QueryText, which is the text in the TextBox, 
+        /// and also ChosenSuggestion, which is only non-null when a user selects an item in the list.</param>
+        private void AutoSuggestName_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            //if (args.ChosenSuggestion != null)
+            //{
+            //    //User selected an item, take an action on it here
+            //    SelectContact(args.ChosenSuggestion as Contact);
+            //}
+            //else
+            //{
+            //    //Do a fuzzy search on the query text
+            //    var matchingContacts = ContactSampleDataSource.GetMatchingContacts(args.QueryText);
+
+            //    if (matchingContacts.Count() >= 1)
+            //    {
+            //        //Choose the first match
+            //        SelectContact(matchingContacts.FirstOrDefault());
+            //    }
+            //    else
+            //    {
+            //        NoResults.Visibility = Visibility.Visible;
+            //    }
+            //}
+        }
+
+
+        /// <summary>
+        /// This event gets fired as the user keys through the list, or taps on a suggestion.
+        /// This allows you to change the text in the TextBox to reflect the item in the list.
+        /// Alternatively you can use TextMemberPath.
+        /// </summary>
+        /// <param name="sender">The AutoSuggestBox that fired the event.</param>
+        /// <param name="args">The args contain SelectedItem, which contains the data item of the item that is currently highlighted.</param>
+        private void AutoSuggestName_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            //var contact = args.SelectedItem as Contact;
+
+            //sender.Text = string.Format("{0} ({1})", contact.FullName, contact.Company);
+        }
+
+        /// <summary>
+        /// This event gets fired anytime the text in the TextBox gets updated.
+        /// It is recommended to check the reason for the text changing by checking against args.Reason
+        /// </summary>
+        /// <param name="sender">The AutoSuggestBox whose text got changed.</param>
+        /// <param name="args">The event arguments.</param>
+        private void AutoSuggestName_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            //We only want to get results when it was a user typing, 
+            //otherwise we assume the value got filled in by TextMemberPath 
+            //or the handler for SuggestionChosen
+            //if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            //{
+            //    var matchingContacts = ContactSampleDataSource.GetMatchingContacts(sender.Text);
+
+            //    sender.ItemsSource = matchingContacts.ToList();
+            //}
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
         }
     }
 }
